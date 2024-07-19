e8e773745987bc1d6fb326d8aa9e00bbd494cc70
UjjTiw <ujjwaltiwari2004razor@gmail.com>
2024-07-19 18:20:31 +0530

    changes

--- train.py
+++ train.py
@@ -1,134 +1,161 @@
 import os
-from typing import Dict, List, Tuple
+import sys
+from collections import defaultdict
+
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
-from pytorch_lightning import LightningModule, Trainer
+
+import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 
+from opt import get_opts
+from datasets import dataset_dict
 from models.nerf import Embedding, NeRF
 from models.rendering import render_rays
-from datasets import dataset_dict
-from utils import get_optimizer, get_scheduler, get_learning_rate
+from utils import get_optimizer, get_scheduler, get_learning_rate, visualize_depth
 from losses import loss_dict
-from metrics import psnr, visualize_depth
+from metrics import psnr
 
-class NeRFSystem(LightningModule):
-    def __init__(self, config: Dict):
-        super().__init__()
-        self.save_hyperparameters(config)
-        self.loss_func = loss_dict[config['loss_type']]()
+class NeRFSystem(pl.LightningModule):
+    def __init__(self, hparams):
+        super(NeRFSystem, self).__init__()
+        self.save_hyperparameters(hparams)
         
-        self.embeddings = nn.ModuleList([
-            Embedding(3, 10),  # xyz embedding
-            Embedding(3, 4)    # direction embedding
-        ])
+        # Loss function
+        self.loss = loss_dict[hparams.loss_type]()
         
-        self.nerf_models = nn.ModuleList([NeRF()])
-        if config['N_importance'] > 0:
-            self.nerf_models.append(NeRF())
-    
-    def forward(self, rays: torch.Tensor) -> Dict[str, torch.Tensor]:
-        return render_rays(
-            self.nerf_models, self.embeddings, rays,
-            self.hparams.N_samples, self.hparams.use_disp,
-            self.hparams.perturb, self.hparams.noise_std,
-            self.hparams.N_importance, self.hparams.chunk,
-            self.train_dataset.white_back
-        )
-    
-    def setup(self, stage: str):
-        dataset_cls = dataset_dict[self.hparams.dataset_name]
-        dataset_kwargs = {
-            'root_dir': self.hparams.root_dir,
-            'img_wh': tuple(self.hparams.img_wh)
-        }
+        # Embeddings
+        self.embedding_xyz = Embedding(3, 10) # 10 is the default number
+        self.embedding_dir = Embedding(3, 4) # 4 is the default number
+        self.embeddings = [self.embedding_xyz, self.embedding_dir]
+
+        # Models
+        self.nerf_coarse = NeRF()
+        self.models = [self.nerf_coarse]
+        if hparams.N_importance > 0:
+            self.nerf_fine = NeRF()
+            self.models += [self.nerf_fine]
+
+    def decode_batch(self, batch):
+        rays = batch['rays']  # (B, 8)
+        rgbs = batch['rgbs']  # (B, 3)
+        return rays, rgbs
+
+    def forward(self, rays):
+        """Do batched inference on rays using chunk."""
+        B = rays.shape[0]
+        results = defaultdict(list)
+        for i in range(0, B, self.hparams.chunk):
+            rendered_ray_chunks = render_rays(
+                self.models,
+                self.embeddings,
+                rays[i:i+self.hparams.chunk],
+                self.hparams.N_samples,
+                self.hparams.use_disp,
+                self.hparams.perturb,
+                self.hparams.noise_std,
+                self.hparams.N_importance,
+                self.hparams.chunk,  # chunk size is effective in val mode
+                self.train_dataset.white_back
+            )
+
+            for k, v in rendered_ray_chunks.items():
+                results[k] += [v]
+
+        for k, v in results.items():
+            results[k] = torch.cat(v, 0)
+        return results
+
+    def prepare_data(self):
+        dataset = dataset_dict[self.hparams.dataset_name]
+        kwargs = {'root_dir': self.hparams.root_dir, 'img_wh': tuple(self.hparams.img_wh)}
         if self.hparams.dataset_name == 'llff':
-            dataset_kwargs.update({
-                'spheric_poses': self.hparams.spheric_poses,
-                'val_num': self.hparams.num_gpus
-            })
-        self.train_dataset = dataset_cls(split='train', **dataset_kwargs)
-        self.val_dataset = dataset_cls(split='val', **dataset_kwargs)
-    
+            kwargs['spheric_poses'] = self.hparams.spheric_poses
+            kwargs['val_num'] = self.hparams.num_gpus
+        self.train_dataset = dataset(split='train', **kwargs)
+        self.val_dataset = dataset(split='val', **kwargs)
+
     def configure_optimizers(self):
-        optimizer = get_optimizer(self.hparams, self.nerf_models)
+        optimizer = get_optimizer(self.hparams, self.models)
         scheduler = get_scheduler(self.hparams, optimizer)
         return [optimizer], [scheduler]
-    
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.train_dataset, shuffle=True, num_workers=4,
-                          batch_size=self.hparams.batch_size, pin_memory=True)
-    
-    def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.val_dataset, shuffle=False, num_workers=4,
-                          batch_size=1, pin_memory=True)
-    
-    def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict:
-        rays, rgbs = batch['rays'], batch['rgbs']
-        results = self(rays)
-        loss = self.loss_func(results, rgbs)
-        
-        typ = 'fine' if 'rgb_fine' in results else 'coarse'
-        psnr_value = psnr(results[f'rgb_{typ}'], rgbs)
-        
-        self.log('train/loss', loss, prog_bar=True)
-        self.log('train/psnr', psnr_value, prog_bar=True)
-        self.log('lr', get_learning_rate(self.optimizers()))
-        
-        return {'loss': loss}
-    
-    def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict:
-        rays, rgbs = batch['rays'].squeeze(), batch['rgbs'].squeeze()
-        results = self(rays)
-        loss = self.loss_func(results, rgbs)
-        
-        typ = 'fine' if 'rgb_fine' in results else 'coarse'
-        psnr_value = psnr(results[f'rgb_{typ}'], rgbs)
-        
-        if batch_idx == 0:
-            W, H = self.hparams.img_wh
-            img = results[f'rgb_{typ}'].view(H, W, 3).permute(2, 0, 1)
-            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1)
-            depth = visualize_depth(results[f'depth_{typ}'].view(H, W))
-            self.logger.experiment.add_images('val/GT_pred_depth',
-                                              torch.stack([img_gt, img, depth]),
-                                              self.global_step)
-        
-        self.log('val/loss', loss, prog_bar=True)
-        self.log('val/psnr', psnr_value, prog_bar=True)
-        
-        return {'val_loss': loss, 'val_psnr': psnr_value}
 
-def main(hparams: Dict):
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, shuffle=True, num_workers=4, batch_size=self.hparams.batch_size, pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, shuffle=False, num_workers=4, batch_size=1, pin_memory=True)
+
+    def training_step(self, batch, batch_nb):
+        rays, rgbs = self.decode_batch(batch)
+        results = self(rays)
+        loss = self.loss(results, rgbs)
+
+        log = {'train/loss': loss, 'lr': get_learning_rate(self.optimizers())}
+        typ = 'fine' if 'rgb_fine' in results else 'coarse'
+        with torch.no_grad():
+            psnr_ = psnr(results[f'rgb_{typ}'], rgbs)
+            log['train/psnr'] = psnr_
+
+        return {'loss': loss, 'log': log, 'progress_bar': {'train_psnr': psnr_}}
+
+    def validation_step(self, batch, batch_nb):
+        rays, rgbs = self.decode_batch(batch)
+        rays = rays.squeeze()  # (H*W, 3)
+        rgbs = rgbs.squeeze()  # (H*W, 3)
+        results = self(rays)
+        loss = self.loss(results, rgbs)
+
+        log = {'val_loss': loss}
+        typ = 'fine' if 'rgb_fine' in results else 'coarse'
+
+        if batch_nb == 0:
+            W, H = self.hparams.img_wh
+            img = results[f'rgb_{typ}'].view(H, W, 3).cpu().permute(2, 0, 1)  # (3, H, W)
+            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu()  # (3, H, W)
+            depth = visualize_depth(results[f'depth_{typ}'].view(H, W))  # (3, H, W)
+            stack = torch.stack([img_gt, img, depth])  # (3, 3, H, W)
+            self.logger.experiment.add_images('val/GT_pred_depth', stack, self.global_step)
+
+        log['val_psnr'] = psnr(results[f'rgb_{typ}'], rgbs)
+        return log
+
+    def validation_epoch_end(self, outputs):
+        mean_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        mean_psnr = torch.stack([x['val_psnr'] for x in outputs]).mean()
+
+        return {'log': {'val/loss': mean_loss, 'val/psnr': mean_psnr}, 'progress_bar': {'val_loss': mean_loss, 'val_psnr': mean_psnr}}
+
+if __name__ == '__main__':
+    hparams = get_opts()
     system = NeRFSystem(hparams)
-    
+
     checkpoint_callback = ModelCheckpoint(
-        dirpath=os.path.join(f'ckpts/{hparams["exp_name"]}'),
+        dirpath=os.path.join('ckpts', hparams.exp_name),
         filename='{epoch:d}',
         monitor='val/loss',
         mode='min',
         save_top_k=5
     )
-    
-    logger = TensorBoardLogger("logs", name=hparams['exp_name'])
-    
-    trainer = Trainer(
-        max_epochs=hparams['num_epochs'],
+
+    logger = TensorBoardLogger(
+        save_dir="logs",
+        name=hparams.exp_name,
+        log_graph=True
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=hparams.num_epochs,
         callbacks=[checkpoint_callback],
-        resume_from_checkpoint=hparams.get('ckpt_path'),
+        resume_from_checkpoint=hparams.ckpt_path,
         logger=logger,
-        gpus=hparams['num_gpus'],
-        strategy='ddp' if hparams['num_gpus'] > 1 else None,
+        weights_summary=None,
+        gpus=hparams.num_gpus,
+        accelerator='ddp' if hparams.num_gpus > 1 else None,
         num_sanity_val_steps=1,
         benchmark=True,
-        profiler="simple" if hparams['num_gpus'] == 1 else None
+        profiler=hparams.num_gpus == 1
     )
-    
-    trainer.fit(system)
 
-if __name__ == '__main__':
-    from opt import get_opts
-    hparams = get_opts()
-    main(hparams)
+    trainer.fit(system)
